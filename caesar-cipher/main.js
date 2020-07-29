1db97424750cd8e74e10009f27f3b6f5afde6c5a
imeubug <im.eubug@gmail.com>
2020-07-29 22:29:11 +0900

    negative shifts added and history

--- caesar-cipher/main.js
+++ caesar-cipher/main.js
@@ -4,8 +4,13 @@ const app = new Vue({
     shift: 1,
     word: '',
     ciphered: '',
+    history: [ 
+    ],
   },
   methods: {
+    addHistory() {
+      this.history.push({text: `${this.word} <${this.shift}> ${this.ciphered}`});
+    },
     caesarCipher() {
       let size = this.word.length;
       let newWord = ''
@@ -23,12 +28,16 @@ const app = new Vue({
       if (charCode >= 65 && charCode <= 90) {
         if (charCode + shift > 90) {
           charCode = 64 + (charCode + shift) % 90;
+        } else if (charCode + shift < 65) {
+          charCode = 91 - (65 - (charCode + shift));
         } else {
           charCode += shift;
         }
       } else if (charCode >= 97 && charCode <= 122) {
         if (charCode + shift > 122) {
           charCode = 96 + (charCode + shift) % 122;
+        } else if (charCode + shift < 97) {
+          charCode = 123 - (97 - (charCode + shift));
         } else {
           charCode += shift;
         }
