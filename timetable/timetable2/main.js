cba969bc7ed06b30635fd02740316a7acee852a5
imeubug <im.eubug@gmail.com>
2020-08-10 16:28:24 +0900

    moved to its own repo

--- timetable/timetable2/main.js
+++
@@ -1,8 +0,0 @@
-function updateClock() {
-  var dt = new Date();
-  document.getElementById("datetime").innerHTML = dt.toLocaleTimeString('en-GB');
-  setTimeout(updateClock, 1000);
-}
-updateClock();
-
-
