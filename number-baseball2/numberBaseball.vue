da271e77c6c4bcc2a5e0b22b9b36cd8fb8143eef
imeubug <im.eubug@gmail.com>
2020-08-13 05:51:13 +0900

    numberbaseball 2 finished

--- number-baseball2/numberBaseball.vue
+++ number-baseball2/numberBaseball.vue
@@ -16,23 +16,65 @@
 </template>
 
 <script>
+  const getNumbers = () => {
+    const candidates = [1 ,2, 3, 4, 5, 6, 7, 8, 9];
+    const arr = [];
+    for (let i=0; i<4; ++i) {
+      const chosen = candidates.splice(Math.floor(Math.random() * (9-i)), 1)[0];
+      arr.push(chosen);
+    }
+    return arr;
+  }
+
   export default {
     data() {
       return {
+        answer: getNumbers(),
         attempts: [],
         value: '',
         result: '',
-      }
+      };
     },
     methods: {
       onSubmitForm(e) {
-        this.attempts.push({
-          try: this.value,
-          result: 'Homerun!',
-        });
+        if (this.value === this.answer.join('')) { // if correct
+          this.attempts.push ({
+            try: this.value, 
+            result: 'Homerun',
+          });
+          this.result = 'Homerun';
+          alert(`Homerun! Yes it's [${this.answer.join(' ')}]`);
+          this.value = '';
+          this.answer = getNumbers();
+          this.attempts = [];
+        } else {
+          if (this.attempts.length >= 9) {
+            this.result = `The answer was [${this.answer.join(' ')}]!`;
+            alert("Starting a new game.");
+            this.value = '';
+            this.answer = getNumbers();
+            this.attempts = [];
+          }
+          let strike = 0;
+          let ball = 0;
+          const answerArray = this.value.split('').map(v => parseInt(v));
+          for (let i=0; i<4; ++i) {
+            if (answerArray[i] === this.answer[i]) { // strike
+              ++strike;
+            } else if (this.answer.includes(answerArray[i])) { // ball
+              ++ball;
+            }
+          }
+
+          this.attempts.push ({
+            try: this.value,
+            result: `Strike: ${strike}   Ball: ${ball}`
+          })
+        }
+
         this.$refs.answer.focus();
         this.value = '';
-        console.log(this.attempts);
+        console.log(this.answer);
       }
     },
   }
