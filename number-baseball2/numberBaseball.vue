4372886db0ba8984cbf6ab2160cdb2870f062a01
imeubug <im.eubug@gmail.com>
2020-08-12 22:14:57 +0900

    numberBaseball 2

---
+++ number-baseball2/numberBaseball.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1> 
+    <form @submit.prevent="onSubmitForm">
+      <input ref="answer" minlength="4" maxlength="4" v-model="value">
+      <button>入力</button>
+    </form>
+    <div>Attempt: {{ attempts.length }}</div>
+    <ul>
+      <li v-for="attempt in attempts">
+        <div>{{ attempt.try }}</div>
+        <div>{{ attempt.result }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        attempts: [],
+        value: '',
+        result: '',
+      }
+    },
+    methods: {
+      onSubmitForm(e) {
+        this.attempts.push({
+          try: this.value,
+          result: 'Homerun!',
+        });
+        this.$refs.answer.focus();
+        this.value = '';
+        console.log(this.attempts);
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
