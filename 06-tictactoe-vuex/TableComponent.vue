1390cf4ab4b51e0931c85f18d0c7bae98155c6d0
Jii Eu <jione.eu@gmail.com>
2020-08-17 16:34:53 +0900

    vuex practice - tictactoe finished

---
+++ 06-tictactoe-vuex/TableComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <table>
+        <tr-component v-for="(row, rowIndex) in tableData" 
+            :key="rowIndex" 
+            :rowIndex="rowIndex">
+        </tr-component>
+    </table>
+</template>
+
+<script>
+import TrComponent from './TrComponent';
+ 
+export default {
+    computed: {
+        tableData() {
+            return this.$store.state.tableData;
+        }
+    },
+    components: {
+        TrComponent,
+    },
+}
+</script>
