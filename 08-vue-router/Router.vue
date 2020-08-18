d74e6bb9d8c1eda5fede0cc53a8935a16a47c151
Jii Eu <jione.eu@gmail.com>
2020-08-18 18:17:10 +0900

    Vue router example

---
+++ 08-vue-router/Router.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <router-link to="/number-baseball">Number Baseball</router-link>
+            <router-link to="/response-check">Response check</router-link>
+            <router-link to="/rockscissorpaper">Rock Scissor Paper</router-link>
+            <router-link to="/lottery-generator">Lottery Generator</router-link>
+            <!-- <router-link to="/tic-tac-toe">Tic Tac Toe</router-link> -->
+            <!-- <router-link to="/minesweeper">Minesweeper</router-link> -->
+
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  
+</style>
